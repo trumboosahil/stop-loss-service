99ad27b2e5b5567e19f17f1c0e650cd772962310
trumboosahil <3725274+trumboosahil@users.noreply.github.com>
2024-10-24 07:23:45 +0400

    updated the fix

--- models/stop_loss_criteria.go
+++ models/stop_loss_criteria.go
@@ -2,9 +2,8 @@ package models
 
 type StopLossCriteria struct {
 	ID            int     `json:"id" gorm:"primaryKey"`
-	OrderID       int     `json:"order_id" gorm:"index"` // Foreign key to the Order
-	StopLossPrice float64 `json:"stop_loss_price"`       // Price at which the stop-loss should be triggered
-	ExpiryDate    int64   `json:"expiry_date"`           // Expiry date for the stop-loss
-	CreatedAt     int64   `json:"created_at"`
+	OrderID       int     `json:"order_id" gorm:"index"`
+	StopLossPrice float64 `json:"stop_loss_price"`
+	ExpiryDate    int64   `json:"expiry_date"`
 	Symbol        string  `json:"symbol"`
 }
